ca6d7fe283b258c289badd5d74a034aee43bcf13
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:56:24 -0600

    Rewriting as Laminas Project package

--- test/CallbackTest.php
+++ test/CallbackTest.php
@@ -1,22 +1,20 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- * @package   Zend_Validator
+ * @see       https://github.com/laminas/laminas-validator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-validator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-validator/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Validator;
+namespace LaminasTest\Validator;
 
-use Zend\Validator\Callback;
+use Laminas\Validator\Callback;
 
 /**
- * @category   Zend
- * @package    Zend_Validator
+ * @category   Laminas
+ * @package    Laminas_Validator
  * @subpackage UnitTests
- * @group      Zend_Validator
+ * @group      Laminas_Validator
  */
 class CallbackTest extends \PHPUnit_Framework_TestCase
 {
@@ -34,7 +32,7 @@ class CallbackTest extends \PHPUnit_Framework_TestCase
     public function testStaticCallback()
     {
         $valid = new Callback(
-            array('\ZendTest\Validator\CallbackTest', 'staticCallback')
+            array('\LaminasTest\Validator\CallbackTest', 'staticCallback')
         );
         $this->assertTrue($valid->isValid('test'));
     }
@@ -64,7 +62,7 @@ class CallbackTest extends \PHPUnit_Framework_TestCase
     {
         $valid = new Callback(array($this, 'objectCallback'));
 
-        $this->setExpectedException('Zend\Validator\Exception\InvalidArgumentException', 'Invalid callback given');
+        $this->setExpectedException('Laminas\Validator\Exception\InvalidArgumentException', 'Invalid callback given');
         $valid->setCallback('invalidcallback');
     }
 
