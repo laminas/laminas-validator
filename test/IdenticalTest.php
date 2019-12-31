cf6481552beea279ca61eb72a2f1d6eaf454d825
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:58:57 -0600

    Rewriting as Laminas Project package

--- test/IdenticalTest.php
+++ test/IdenticalTest.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-validator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-validator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-validator/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Validator;
+namespace LaminasTest\Validator;
 
-use Zend\Validator\Identical;
+use Laminas\Validator\Identical;
 
 /**
- * @group      Zend_Validator
+ * @group      Laminas_Validator
  */
 class IdenticalTest extends \PHPUnit_Framework_TestCase
 {
@@ -74,7 +73,7 @@ class IdenticalTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @group ZF-6953
+     * @group Laminas-6953
      */
     public function testValidatingAgainstEmptyToken()
     {
@@ -83,7 +82,7 @@ class IdenticalTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @group ZF-7128
+     * @group Laminas-7128
      */
     public function testValidatingAgainstNonStrings()
     {
