c7d514ac3d7d30b8855e47c50b1bf82606175181
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:59:26 -0600

    Rewriting as Laminas Project package

--- src/EmailAddress.php
+++ src/EmailAddress.php
@@ -1,13 +1,12 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/zf2 for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-validator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-validator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-validator/blob/master/LICENSE.md New BSD License
  */
 
-namespace Zend\Validator;
+namespace Laminas\Validator;
 
 class EmailAddress extends AbstractValidator
 {
@@ -55,7 +54,7 @@ class EmailAddress extends AbstractValidator
     protected $localPart;
 
     /**
-     * Returns the found mx record informations
+     * Returns the found mx record information
      *
      * @var array
      */
@@ -76,8 +75,8 @@ class EmailAddress extends AbstractValidator
      * Instantiates hostname validator for local use
      *
      * The following additional option keys are supported:
-     * 'hostnameValidator' => A hostname validator, see Zend\Validator\Hostname
-     * 'allow'             => Options for the hostname validator, see Zend\Validator\Hostname::ALLOW_*
+     * 'hostnameValidator' => A hostname validator, see Laminas\Validator\Hostname
+     * 'allow'             => Options for the hostname validator, see Laminas\Validator\Hostname::ALLOW_*
      * 'useMxCheck'        => If MX check should be enabled, boolean
      * 'useDeepMxCheck'    => If a deep MX check should be done, boolean
      *
@@ -470,7 +469,7 @@ class EmailAddress extends AbstractValidator
     }
 
     /**
-     * Defined by Zend\Validator\ValidatorInterface
+     * Defined by Laminas\Validator\ValidatorInterface
      *
      * Returns true if and only if $value is a valid email address
      * according to RFC2822
