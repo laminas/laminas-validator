46e3b96b241f4f12298ff1433d0b66e0538a699f
Michał Bundyra <contact@webimpress.com>
2020-03-31 18:17:10 +0100

    Adds missing doc header in the test asset file
    Signed-off-by: Michał Bundyra <contact@webimpress.com>

--- test/TestAsset/CustomTraversable.php
+++ test/TestAsset/CustomTraversable.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @see       https://github.com/laminas/laminas-validator for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-validator/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-validator/blob/master/LICENSE.md New BSD License
+ */
+
 namespace LaminasTest\Validator\TestAsset;
 
 use Iterator;
