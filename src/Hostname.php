7bee1dbe61c5634cc1e46089c877a56b8d1d7180
Aleksei Khudiakov <aleksey@xerkus.pro>
2023-06-12 19:18:23 +1000

    Drop IanaVersion from Hostname validator comment
    Signed-off-by: Aleksei Khudiakov <aleksey@xerkus.pro>

--- src/Hostname.php
+++ src/Hostname.php
@@ -89,7 +89,6 @@ class Hostname extends AbstractValidator
 
     /**
      * Array of valid top-level-domains
-     * IanaVersion 2023061100
      *
      * @see ftp://data.iana.org/TLD/tlds-alpha-by-domain.txt  List of all TLDs by domain
      * @see http://www.iana.org/domains/root/db/ Official list of supported TLDs
