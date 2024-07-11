490d6e2882cd704cc434f7450dd4500b5bded810
George Steel <george@net-glue.co.uk>
2024-07-11 11:30:23 +0100

    Refactor `Extension` and `ExcludeExtension` Validators
    - Remove compat with legacy `Laminas\File\Transfer` api
    - Remove all setters and getters
    - Adjust inheritance so both classes can be final
    - Only accept an options array to the constructor
    - Add new error message for completely invalid values (non-string, non-upload) instead of throwing an exception
    
    Signed-off-by: George Steel <george@net-glue.co.uk>

--- test/File/ExtensionTest.php
+++ test/File/ExtensionTest.php
@@ -4,237 +4,207 @@ declare(strict_types=1);
 
 namespace LaminasTest\Validator\File;
 
+use Laminas\Diactoros\UploadedFile;
 use Laminas\Validator\Exception\InvalidArgumentException;
 use Laminas\Validator\File\Extension;
 use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
 
-use function array_merge;
-use function basename;
-use function current;
-use function is_array;
+use const UPLOAD_ERR_OK;
 
-use const UPLOAD_ERR_NO_FILE;
-
+/** @psalm-import-type OptionsArgument from Extension */
 final class ExtensionTest extends TestCase
 {
     /**
-     * @psalm-return array<array-key, array{
-     *     0: string|string[],
-     *     1: string|array{
-     *         tmp_name: string,
-     *         name: string,
-     *         size: int,
-     *         error: int,
-     *         type: string
-     *     },
+     * @psalm-return iterable<string, array{
+     *     0: OptionsArgument,
+     *     1: mixed,
      *     2: bool,
-     *     3: string
+     *     3: string|null,
      * }>
      */
-    public static function basicBehaviorDataProvider(): array
+    public static function basicBehaviorDataProvider(): iterable
     {
-        $testFile     = __DIR__ . '/_files/testsize.mo';
-        $pictureTests = [
-            //    Options, isValid Param, Expected value, Expected message
-            ['mo',                  $testFile, true,  ''],
-            ['gif',                 $testFile, false, 'fileExtensionFalse'],
-            [['mo'], $testFile, true, ''],
-            [['gif'], $testFile, false, 'fileExtensionFalse'],
-            [['gif', 'mo', 'pict'], $testFile, true, ''],
-            [['gif', 'gz', 'hint'], $testFile, false, 'fileExtensionFalse'],
+        yield 'String filename found in allowed list' => [
+            ['extension' => 'tar.gz', 'case' => false, 'allowNonExistentFile' => true],
+            'SomeFile.tar.gz',
+            true,
+            null,
         ];
 
-        $testFile    = __DIR__ . '/_files/nofile.mo';
-        $noFileTests = [
-            //    Options, isValid Param, Expected value, message
-            ['mo', $testFile, false, 'fileExtensionNotFound'],
-            [['extension' => 'mo', 'allowNonExistentFile' => true], $testFile, true, ''],
+        yield 'String filename found in allowed list, mismatched case' => [
+            ['extension' => 'GZ,ZIP', 'case' => true, 'allowNonExistentFile' => true],
+            'SomeFile.tar.gz',
+            false,
+            Extension::FALSE_EXTENSION,
         ];
 
-        // Dupe data in File Upload format
-        $testData = array_merge($pictureTests, $noFileTests);
-        foreach ($testData as $data) {
-            $fileUpload = [
-                'tmp_name' => $data[1],
-                'name'     => basename($data[1]),
-                'size'     => 200,
-                'error'    => 0,
-                'type'     => 'text',
-            ];
-            $testData[] = [$data[0], $fileUpload, $data[2], $data[3]];
-        }
+        yield 'String filename found in allowed list, matched case' => [
+            ['extension' => 'GZ,ZIP', 'case' => true, 'allowNonExistentFile' => true],
+            'SHOUTING.ZIP',
+            true,
+            null,
+        ];
 
-        return $testData;
+        yield 'String filename not found in allowed list' => [
+            ['extension' => 'zip,txt,pdf', 'case' => false, 'allowNonExistentFile' => true],
+            'SomeFile.tar.gz',
+            false,
+            Extension::FALSE_EXTENSION,
+        ];
+
+        yield 'Non-existent file as string' => [
+            ['extension' => 'txt'],
+            'Some-File.txt',
+            false,
+            Extension::NOT_FOUND,
+        ];
+
+        yield 'Non string and non upload value' => [
+            ['extension' => 'txt', 'allowNonExistentFile' => true],
+            true,
+            false,
+            Extension::ERROR_INVALID_TYPE,
+        ];
+
+        $existingFileAsUploadArray = [
+            'tmp_name' => __DIR__ . '/_files/picture.jpg',
+            'name'     => 'picture.jpg',
+            'size'     => 200,
+            'error'    => UPLOAD_ERR_OK,
+            'type'     => 'image/jpeg',
+        ];
+
+        yield 'PHP Upload with real file and matching extension' => [
+            ['extension' => 'jpg,png,gif'],
+            $existingFileAsUploadArray,
+            true,
+            null,
+        ];
+
+        yield 'PHP Upload with real file and non-matching extension' => [
+            ['extension' => 'txt,pdf'],
+            $existingFileAsUploadArray,
+            false,
+            Extension::FALSE_EXTENSION,
+        ];
+
+        $psrUpload = new UploadedFile(
+            __DIR__ . '/_files/picture.jpg',
+            200,
+            UPLOAD_ERR_OK,
+            'picture.jpg',
+            'image/jpeg',
+        );
+
+        yield 'PSR Upload with real file and matching extension' => [
+            ['extension' => 'jpg,png,gif'],
+            $psrUpload,
+            true,
+            null,
+        ];
+
+        yield 'PSR Upload with real file and non-matching extension' => [
+            ['extension' => 'txt,pdf'],
+            $psrUpload,
+            false,
+            Extension::FALSE_EXTENSION,
+        ];
     }
 
     /**
      * Ensures that the validator follows expected behavior
      *
-     * @param string|string[] $options,
-     * @param string|array $isValidParam
+     * @param OptionsArgument $options
      */
     #[DataProvider('basicBehaviorDataProvider')]
-    public function testBasic($options, $isValidParam, bool $expected, string $messageKey): void
+    public function testBasic(array $options, mixed $value, bool $expected, string|null $messageKey): void
     {
         $validator = new Extension($options);
 
-        self::assertSame($expected, $validator->isValid($isValidParam));
+        self::assertSame($expected, $validator->isValid($value));
 
-        if (! $expected) {
+        if (! $expected && $messageKey !== null) {
             self::assertArrayHasKey($messageKey, $validator->getMessages());
         }
     }
 
-    /**
-     * Ensures that the validator follows expected behavior for legacy Laminas\Transfer API
-     *
-     * @param string|string[] $options,
-     * @param string|array $isValidParam
-     */
-    #[DataProvider('basicBehaviorDataProvider')]
-    public function testLegacy($options, $isValidParam, bool $expected, string $messageKey): void
+    public function testExtensionListIsARequiredOption(): void
     {
-        if (! is_array($isValidParam)) {
-            self::markTestSkipped('An array is expected for legacy compat tests');
-        }
-
-        $validator = new Extension($options);
-
-        self::assertSame($expected, $validator->isValid($isValidParam['tmp_name'], $isValidParam));
-
-        if (! $expected) {
-            self::assertArrayHasKey($messageKey, $validator->getMessages());
-        }
-    }
-
-    public function testLaminas891(): void
-    {
-        $files     = [
-            'name'     => 'testsize.mo',
-            'type'     => 'text',
-            'size'     => 200,
-            'tmp_name' => __DIR__ . '/_files/testsize.mo',
-            'error'    => 0,
-        ];
-        $validator = new Extension(['MO', 'case' => true]);
-
-        self::assertFalse($validator->isValid(__DIR__ . '/_files/testsize.mo', $files));
-
-        $validator = new Extension(['MO', 'case' => false]);
-
-        self::assertTrue($validator->isValid(__DIR__ . '/_files/testsize.mo', $files));
-    }
-
-    /** @psalm-return array<array{string|string[], string[]}> */
-    public static function getExtensionProvider(): array
-    {
-        return [
-            ['mo', ['mo']],
-            [['mo', 'gif', 'jpg'], ['mo', 'gif', 'jpg']],
-        ];
-    }
-
-    /**
-     * Ensures that getExtension() returns expected value
-     *
-     * @param string|string[] $extension
-     * @param string[] $expected
-     */
-    #[DataProvider('getExtensionProvider')]
-    public function testGetExtension($extension, array $expected): void
-    {
-        $validator = new Extension($extension);
-
-        self::assertSame($expected, $validator->getExtension());
-    }
-
-    /** @psalm-return array<array{string|string[], string[]}> */
-    public static function setExtensionProvider(): array
-    {
-        return [
-            ['gif', ['gif']],
-            ['jpg, mo', ['jpg', 'mo']],
-            [['zip', 'ti'], ['zip', 'ti']],
-        ];
-    }
-
-    /**
-     * Ensures that setExtension() returns expected value
-     *
-     * @param string|string[] $extension
-     * @param string[] $expected
-     */
-    #[DataProvider('setExtensionProvider')]
-    public function testSetExtension($extension, array $expected): void
-    {
-        $validator = new Extension('mo');
-        $validator->setExtension($extension);
-
-        self::assertSame($expected, $validator->getExtension());
-    }
-
-    /**
-     * Ensures that addExtension() returns expected value
-     */
-    public function testAddExtension(): void
-    {
-        $validator = new Extension('mo');
-        $validator->addExtension('gif');
-
-        self::assertSame(['mo', 'gif'], $validator->getExtension());
-
-        $validator->addExtension('jpg, to');
-
-        self::assertSame(['mo', 'gif', 'jpg', 'to'], $validator->getExtension());
-
-        $validator->addExtension(['zip', 'ti']);
-
-        self::assertSame(['mo', 'gif', 'jpg', 'to', 'zip', 'ti'], $validator->getExtension());
-
-        $validator->addExtension('');
-
-        self::assertSame(['mo', 'gif', 'jpg', 'to', 'zip', 'ti'], $validator->getExtension());
-    }
-
-    #[Group('Laminas-11258')]
-    public function testLaminas11258(): void
-    {
-        $validator = new Extension('gif');
-
-        self::assertFalse($validator->isValid(__DIR__ . '/_files/nofile.mo'));
-        self::assertArrayHasKey('fileExtensionNotFound', $validator->getMessages());
-        self::assertStringContainsString('does not exist', current($validator->getMessages()));
-    }
-
-    public function testEmptyFileShouldReturnFalseAndDisplayNotFoundMessage(): void
-    {
-        $validator = new Extension('foo');
-
-        self::assertFalse($validator->isValid(''));
-        self::assertArrayHasKey(Extension::NOT_FOUND, $validator->getMessages());
-
-        $filesArray = [
-            'name'     => '',
-            'size'     => 0,
-            'tmp_name' => '',
-            'error'    => UPLOAD_ERR_NO_FILE,
-            'type'     => '',
-        ];
-
-        self::assertFalse($validator->isValid($filesArray));
-        self::assertArrayHasKey(Extension::NOT_FOUND, $validator->getMessages());
-    }
-
-    public function testIsValidRaisesExceptionForArrayNotInFilesFormat(): void
-    {
-        $validator = new Extension('foo');
-        $value     = ['foo' => 'bar'];
-
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('Value array must be in $_FILES format');
+        $this->expectExceptionMessage('The extension option must resolve to a non-empty list');
 
-        $validator->isValid($value);
+        /** @psalm-suppress InvalidArgument */
+        new Extension([]);
+    }
+
+    /** @return array<string, array{0: string|list<string>, 1: list<string>}> */
+    public static function extensionOptions(): array
+    {
+        return [
+            'CSV'                   => ['foo,bar,baz', ['foo', 'bar', 'baz']],
+            'CSV with empty values' => ['foo,,,bar', ['foo', 'bar']],
+            'Single string'         => ['baz', ['baz']],
+            'Single array element'  => [['foo'], ['foo']],
+            'List'                  => [['a', 'b', 'c'], ['a', 'b', 'c']],
+            'List with empties'     => [['a', ''], ['a']],
+        ];
+    }
+
+    /**
+     * @param string|array<array-key, string> $input
+     * @param list<string> $expect
+     * @psalm-suppress InternalMethod
+     */
+    #[DataProvider('extensionOptions')]
+    public function testResolveExtensionList(string|array $input, array $expect): void
+    {
+        self::assertSame($expect, Extension::resolveExtensionList($input));
+    }
+
+    /** @return array<string, array{0: string|array<array-key, string>}> */
+    public static function invalidExtensionOptions(): array
+    {
+        return [
+            'Empty Array'                  => [[]],
+            'Only Commas'                  => [',,,,'],
+            'Empty String'                 => [''],
+            'List with only empty strings' => [['', '']],
+        ];
+    }
+
+    /**
+     * @param string|array<array-key, string> $list
+     * @psalm-suppress InternalMethod
+     */
+    #[DataProvider('invalidExtensionOptions')]
+    public function testResolveExtensionListIsExceptional(string|array $list): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('The extension option must resolve to a non-empty list');
+
+        Extension::resolveExtensionList($list);
+    }
+
+    /** @return list<array{0: non-empty-string, 1: list<string>}> */
+    public static function fileNameExtensionProvider(): array
+    {
+        return [
+            ['file.tar.gz', ['gz', 'tar.gz']],
+            ['file...txt', ['txt']],
+            ['file..tar...gz', ['gz', 'tar.gz']],
+            ['file.bar.baz.pdf', ['pdf', 'baz.pdf', 'bar.baz.pdf']],
+        ];
+    }
+
+    /**
+     * @param non-empty-string $input
+     * @param list<string> $expect
+     * @psalm-suppress InternalMethod
+     */
+    #[DataProvider('fileNameExtensionProvider')]
+    public function testListPossibleFileNameExtensions(string $input, array $expect): void
+    {
+        self::assertSame($expect, Extension::listPossibleFileNameExtensions($input));
     }
 }
